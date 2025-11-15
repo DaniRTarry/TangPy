71908d0db732ee46a6ea1df325a574e6418eaa8e
Daniel Rodríguez Tarry <drtarry@uw.edu>
2025-11-14 18:42:33 -0800

    Reorganization and cleaning
    Optimization, better handling of variables and functions and better readability

--- tang_model_plot.py
+++ tang_model_plot.py
@@ -1,118 +1,159 @@
-# -*- coding: utf-8 -*-
 """
-@author: Tarry
+Plotting functions for tangmodel.py
 
-type: FUNCTION
-
-description: function which contains all the plottings required in tangmodel.py
-
-input: x grid, y grid, day, plotfield, plotlabel
+Author: Tarry
+Type: FUNCTION
+Description: Contains all plotting functions required in tangmodel.py
 """
 
-# Load libraries
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-# Figures out the absolute path in case the working directory
-# my_path = os.path.abspath(__file__)
 
-# A = os.path.join(os.path.dirname(__file__), '..')
-# A is the parent directory of the directory where program resides.
-
-# B = os.path.dirname(os.path.realpath(__file__))
-# B is the canonicalised (?) directory where the program resides.
-
-# C = os.path.abspath(os.path.dirname(__file__))
-# C is the absolute path of the directory where the program resides.
+# Constants
+KM_CONVERSION = 1000  # Conversion from meters to kilometers
 
 
-def tm_cfplot(x_grid,y_grid,day,plotfield,levels,plotlabel,units,cmap,image_dir):
-
-	plt.cla
-	plt.clf
-	plt.close()
-	plt.figure()
-
-	# Filled Contour plot
-	cs = plt.contourf(x_grid/1000, y_grid/1000, plotfield, levels=levels ,cmap=cmap)
-	# Contour lines on top
-	#cs2= plt.contour(x_grid/1000, y_grid/1000, plotfield, colors='k', linewidths=1)
-	#plt.clabel(cs2, inline=True, fmt="%0.3f", fontsize=10, colors="black")
-	# Colorbar
-	cbar = plt.colorbar(cs, shrink=1.0, extend='both',ticks=np.linspace(levels[0],abs(levels[0]),11))
-	cbar.ax.set_ylabel(plotlabel+' ('+units+')')
-
-	plt.title('Model '+plotlabel+' d'+str(day))
-	plt.ylabel('y (km)')
-	plt.xlabel('x (km)')
-
-	image_name = 'model_'+plotlabel+'_d'+str("{0:02d}".format(day))
-	filename = image_dir+image_name+'.pdf'
-	plt.savefig(filename,bbox_inches='tight')
-	filename = image_dir+image_name+'.png'
-	plt.savefig(filename,bbox_inches='tight')
-
-	plt.close()
+def _save_figure(image_dir, image_name, formats=('pdf', 'png')):
+    """Helper function to save figures in multiple formats."""
+    for fmt in formats:
+        filename = os.path.join(image_dir, f"{image_name}.{fmt}")
+        plt.savefig(filename, bbox_inches='tight')
 
 
-
-def tm_vec_cfplot(x_grid,y_grid,day,vecfield,veclabel,vecunits,r,length,scale,plotfield,plotlabel,levels,plotunits,cmap,image_dir):
-
-	plt.cla
-	plt.clf
-	plt.close()
-	plt.figure()
-
-	# Filled Contour plot
-	cs = plt.contourf(x_grid/1000, y_grid/1000, plotfield, levels=levels ,cmap=cmap)
-	cbar = plt.colorbar(cs, shrink=1.0, extend='both',ticks=np.linspace(levels[0],abs(levels[0]),11))
-	cbar.ax.set_ylabel(plotlabel+' ('+plotunits+')')
-
-	# Vector plot:
-	# To obtain a clearer vector map will plot vectors each r data point:
-	Q = plt.quiver(x_grid[::r,::r][0,:]/1000,y_grid[::r,::r][:,0]/1000,vecfield[0][::r,::r],vecfield[1][::r,::r],scale=scale,headwidth=4,headlength=4)
-	qk = plt.quiverkey(Q, 0.65, 0.92, length, str(length)+vecunits, labelpos='E',coordinates='figure')
-
-	plt.title('Model '+veclabel+'+'+plotlabel+' d'+str("{0:02d}".format(day)))
-	plt.ylabel('y (km)')
-	plt.xlabel('x (km)')
-
-	image_name = 'model_'+veclabel+'+'+plotlabel+'_d'+str("{0:02d}".format(day))+'.pdf'
-	filename = image_dir+image_name
-	plt.savefig(filename,bbox_inches='tight')
-  	image_name = 'model_'+veclabel+'+'+plotlabel+'_d'+str("{0:02d}".format(day))+'.png'
-	filename = image_dir+image_name
-	plt.savefig(filename,bbox_inches='tight')
-
-	plt.close()
+def _setup_colorbar(cs, label, units, levels):
+    """Helper function to create and configure colorbar."""
+    ticks = np.linspace(levels[0], abs(levels[0]), 11)
+    cbar = plt.colorbar(cs, shrink=1.0, extend='both', ticks=ticks)
+    cbar.ax.set_ylabel(f'{label} ({units})')
+    return cbar
 
 
+def tm_cfplot(x_grid, y_grid, day, plotfield, levels, plotlabel, units, cmap, image_dir):
+    """
+    Create filled contour plot.
+    
+    Parameters:
+        x_grid: X coordinate grid
+        y_grid: Y coordinate grid
+        day: Day number for plot title
+        plotfield: Data field to plot
+        levels: Contour levels
+        plotlabel: Label for the plot
+        units: Units for the data
+        cmap: Colormap
+        image_dir: Directory to save images
+    """
+    plt.close('all')
+    plt.figure()
+    
+    # Filled contour plot
+    cs = plt.contourf(x_grid / KM_CONVERSION, y_grid / KM_CONVERSION, 
+                      plotfield, levels=levels, cmap=cmap)
+    
+    # Colorbar
+    _setup_colorbar(cs, plotlabel, units, levels)
+    
+    # Labels and title
+    plt.title(f'Model {plotlabel} d{day}')
+    plt.ylabel('y (km)')
+    plt.xlabel('x (km)')
+    
+    # Save figure
+    image_name = f'model_{plotlabel}_d{day:02d}'
+    _save_figure(image_dir, image_name)
+    plt.close()
 
-def tm_vecplot(x_grid,y_grid,day,vecfield,veclabel,r,length,scale,units,image_dir):
-	plt.cla
-	plt.clf
-	plt.close()
-	plt.figure()
 
-	# Vector plot:
-	# To obtain a clearer vector map will plot vectors each r data point:
-	Q = plt.quiver(x_grid[::r,::r]/1000,y_grid[::r,::r]/1000,vecfield[0][::r,::r],vecfield[1][::r,::r],scale=scale)
-	qk = plt.quiverkey(Q, 0.9, 0.95, length, str(length)+units, labelpos='E',coordinates='figure')
+def tm_vec_cfplot(x_grid, y_grid, day, vecfield, veclabel, vecunits, r, 
+                  length, scale, plotfield, plotlabel, levels, plotunits, 
+                  cmap, image_dir):
+    """
+    Create combined vector and filled contour plot.
+    
+    Parameters:
+        x_grid: X coordinate grid
+        y_grid: Y coordinate grid
+        day: Day number for plot title
+        vecfield: Vector field data [u, v]
+        veclabel: Label for vector field
+        vecunits: Units for vector field
+        r: Stride for vector plotting (plot every r-th point)
+        length: Reference vector length
+        scale: Scale for quiver plot
+        plotfield: Background contour field
+        plotlabel: Label for contour field
+        levels: Contour levels
+        plotunits: Units for contour field
+        cmap: Colormap
+        image_dir: Directory to save images
+    """
+    plt.close('all')
+    plt.figure()
+    
+    # Filled contour plot
+    cs = plt.contourf(x_grid / KM_CONVERSION, y_grid / KM_CONVERSION, 
+                      plotfield, levels=levels, cmap=cmap)
+    _setup_colorbar(cs, plotlabel, plotunits, levels)
+    
+    # Vector plot (subsample for clarity)
+    Q = plt.quiver(x_grid[::r, ::r][0, :] / KM_CONVERSION,
+                   y_grid[::r, ::r][:, 0] / KM_CONVERSION,
+                   vecfield[0][::r, ::r],
+                   vecfield[1][::r, ::r],
+                   scale=scale, headwidth=4, headlength=4)
+    
+    plt.quiverkey(Q, 0.65, 0.92, length, f'{length}{vecunits}', 
+                  labelpos='E', coordinates='figure')
+    
+    # Labels and title
+    plt.title(f'Model {veclabel}+{plotlabel} d{day:02d}')
+    plt.ylabel('y (km)')
+    plt.xlabel('x (km)')
+    
+    # Save figure
+    image_name = f'model_{veclabel}+{plotlabel}_d{day:02d}'
+    _save_figure(image_dir, image_name)
+    plt.close()
 
-	plt.title('Model '+veclabel+' d'+str("{0:02d}".format(day)))
-	plt.ylabel('y (km)')
-	plt.xlabel('x (km)')
 
-	image_name = 'model_'+veclabel+'_d'+str("{0:02d}".format(day))
-	filename = image_dir+image_name+'.pdf'
-	plt.savefig(filename,bbox_inches='tight')
-	filename = image_dir+image_name+'.png'
-	plt.savefig(filename,bbox_inches='tight')
-
-	plt.close()
-
-
-
-
-
+def tm_vecplot(x_grid, y_grid, day, vecfield, veclabel, r, length, 
+               scale, units, image_dir):
+    """
+    Create vector plot.
+    
+    Parameters:
+        x_grid: X coordinate grid
+        y_grid: Y coordinate grid
+        day: Day number for plot title
+        vecfield: Vector field data [u, v]
+        veclabel: Label for vector field
+        r: Stride for vector plotting (plot every r-th point)
+        length: Reference vector length
+        scale: Scale for quiver plot
+        units: Units for vector field
+        image_dir: Directory to save images
+    """
+    plt.close('all')
+    plt.figure()
+    
+    # Vector plot (subsample for clarity)
+    Q = plt.quiver(x_grid[::r, ::r] / KM_CONVERSION,
+                   y_grid[::r, ::r] / KM_CONVERSION,
+                   vecfield[0][::r, ::r],
+                   vecfield[1][::r, ::r],
+                   scale=scale)
+    
+    plt.quiverkey(Q, 0.9, 0.95, length, f'{length}{units}', 
+                  labelpos='E', coordinates='figure')
+    
+    # Labels and title
+    plt.title(f'Model {veclabel} d{day:02d}')
+    plt.ylabel('y (km)')
+    plt.xlabel('x (km)')
+    
+    # Save figure
+    image_name = f'model_{veclabel}_d{day:02d}'
+    _save_figure(image_dir, image_name)
+    plt.close()
